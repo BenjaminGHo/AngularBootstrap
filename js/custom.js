294168f8ffa8754596767824eaf9b6e362f4137a
Ben Ho <ben@email.com>
2017-12-05 15:30:45 -0500

    added new link to view JSON feed for specific game

--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,26 @@
 var app = angular.module('myApp', []);
 
+/* testing for first controller
 app.controller('customersCtrl', function($scope, $http) {
 	$http.get("https://www.w3schools.com/angular/customers.php")
 	.then(function (response) {
 		$scope.names = response.data.records;
 	});
 });
+*/
 
 app.controller('nflCtrl', function($scope, $http) {
 	$http.get("http://www.nfl.com/liveupdate/scorestrip/ss.xml")
 	.then(function (response) {
-		xmlDoc  = $.parseXML(response.data);
-		$stringToXMLObject = $(xmlDoc);
 		
 		var jsonList = [];
-		var team;
+		var team, xmlDoc;
 		
+		xmlDoc = $.parseXML(response.data);
+		$stringToXMLObject = $(xmlDoc);
 		$stringToXMLObject.find("ss").find("gms").find("g").each(function()
 		{
-			team = {homeTeamName: $(this).attr("hnn"), visitTeamName: $(this).attr("vnn")};
+			team = {homeTeamName: $(this).attr("hnn"), visitTeamName: $(this).attr("vnn"), eid: $(this).attr("eid")};
 			jsonList.push(team);
 		})
 
