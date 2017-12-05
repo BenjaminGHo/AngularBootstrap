2fa5d128a7f2a835c019c1405705db9b71e201f9
Ben Ho <ben@email.com>
2017-12-05 14:24:31 -0500

    included ajax call to NFL to grab incoming games and display results

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,29 @@
 var app = angular.module('myApp', []);
+
 app.controller('customersCtrl', function($scope, $http) {
 	$http.get("https://www.w3schools.com/angular/customers.php")
 	.then(function (response) {
 		$scope.names = response.data.records;
-		console.log(response);
+	});
+});
+
+app.controller('nflCtrl', function($scope, $http) {
+	$http.get("http://www.nfl.com/liveupdate/scorestrip/ss.xml")
+	.then(function (response) {
+		xmlDoc  = $.parseXML(response.data);
+		$stringToXMLObject = $(xmlDoc);
+		
+		var jsonList = [];
+		var team;
+		
+		$stringToXMLObject.find("ss").find("gms").find("g").each(function()
+		{
+			team = {homeTeamName: $(this).attr("hnn"), visitTeamName: $(this).attr("vnn")};
+			jsonList.push(team);
+		})
+
+		$scope.teams = jsonList;
+	})
+	.catch(function(data) {
 	});
 });
